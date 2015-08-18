0cc2540f35674651c2f4b01b41a1a042a2533b64
Ethan Jewett <esjewett@gmail.com>
2015-08-18 16:58:43 -0500

    Put worker logic into named functions for profiling

--- src/main/webapp/js/cf-worker.js
+++ src/main/webapp/js/cf-worker.js
@@ -3,44 +3,74 @@ importScripts('crossfilter.js', 'reductio.js', 'd3.js', 'dc.js');
 var vars = {};
 
 onmessage = function(e) {
-  var temp, func;
-  
   switch(e.data.type) {
-    case 'reductio_var': vars[e.data.id] = reductio();
+    case 'reductio_var':
+      reductio_var(e);
       break;
-    case 'crossfilter_var': vars[e.data.id] = crossfilter();
+    case 'crossfilter_var':
+      crossfilter_var(e);
       break;
     case 'var_methods':
-      temp = e.data.methods.reduce(function(r,m) {
-        return r[m.method].apply(null, m.args);
-      }, vars[e.data.id]);
-      if(e.data.newId !== undefined) {
-        vars[e.data.newId] = temp;
-      }
+      var_methods(e);
       break;
     case 'var_method_function':
-      func = unpackFunction(e.data.func, e.data.context)
-      temp = vars[e.data.id][e.data.method].call(null, func);
-      if(e.data.newId !== undefined) {
-        vars[e.data.newId] = temp;
-      }
+      var_method_function(e);
       break;
     case 'call_var_on_var':
-      vars[e.data.callFunc].call(null, vars[e.data.arg]);
+      call_var_on_var(e)
       break;
     case 'var_method_return':
-      var dat = vars[e.data.id][e.data.method].call(null, e.data.arg);
-      dat.return_id = e.data.return_id;
-      postMessage(dat);
+      var_method_return(e);
       break;
     case 'var_unstructured_method_return':
-      var dat = vars[e.data.id][e.data.method].call(null, e.data.arg);
-      var strct = {};
-      strct.data = dat;
-      strct.return_id = e.data.return_id;
-      postMessage(strct);
+      var_unstructured_method_return(e);
       break;
   }
+}
+
+function reductio_var(e) {
+  vars[e.data.id] = reductio();
+}
+
+function crossfilter_var(e) {
+  vars[e.data.id] = crossfilter();
+}
+
+function var_methods(e) {
+  var temp;
+  temp = e.data.methods.reduce(function(r,m) {
+    return r[m.method].apply(null, m.args);
+  }, vars[e.data.id]);
+  if(e.data.newId !== undefined) {
+    vars[e.data.newId] = temp;
+  }
+}
+
+function var_method_function(e) {
+  var temp, func;
+  func = unpackFunction(e.data.func, e.data.context)
+  temp = vars[e.data.id][e.data.method].call(null, func);
+  if(e.data.newId !== undefined) {
+    vars[e.data.newId] = temp;
+  }
+}
+
+function call_var_on_var(e) {
+  vars[e.data.callFunc].call(null, vars[e.data.arg]);
+}
+
+function var_method_return(e) {
+  var dat = vars[e.data.id][e.data.method].call(null, e.data.arg);
+  dat.return_id = e.data.return_id;
+  postMessage(dat);
+}
+
+function var_unstructured_method_return(e) {
+  var dat = vars[e.data.id][e.data.method].call(null, e.data.arg);
+  var strct = {};
+  strct.data = dat;
+  strct.return_id = e.data.return_id;
+  postMessage(strct);
 }
 
 function unpackFunction(func, context) {
